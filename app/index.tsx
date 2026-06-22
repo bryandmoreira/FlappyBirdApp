336d559e470eef98b7a41f5e8dfd662075bd136a
bryandmoreira <bryan.d.moreira@aluno.senai.br>
2026-06-22 17:19:57 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.screen}>
+            <Text style={styles.title}>Flappy Bird</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "black",
+         alignItems: "center"
+    },
+
+    title: {
+        fontSize: 50,
+        fontWeight: "bold",
+        color: "#fff",
+        marginTop: 100,
+        
+       
+        borderWidth: 1,
+        borderColor: "#fff",
+       
+
+
+    },
+});
